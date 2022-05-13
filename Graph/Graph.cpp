1b145862c76f2604a9d4f867346279a5c5a9eeb1
Vladislav Nekrasov <vladislav.nekrasov123@gmail.com>
2022-05-13 18:11:09 +0300

    added more graphs

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <typeinfo>
 
 void adjMatrix(int** adj, int size, bool type)
 {
@@ -10,7 +12,7 @@ void adjMatrix(int** adj, int size, bool type)
 	{
 		for (int j = 0; j < size; j++)
 		{
-			if (adj[i][j] == 0 && i != j)
+			if (adj[i][j] == 0)
 			{
 				std::cout << "Vertex " << i + 1 << ".\nEdge weight for vertex N" << j + 1 << ".\n" <<
 					"Type 0 for no edge.\nInput: ";
@@ -26,7 +28,8 @@ void adjMatrix(int** adj, int size, bool type)
 	}
 }
 
-void print(int** adj, int size, int width)
+template <class T, class U>
+void print(T adj, U size, U width)
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -35,6 +38,7 @@ void print(int** adj, int size, int width)
 		std::cout << "\n";
 	}
 }
+
 
 int mostConnected(int** adj, int size)
 {
@@ -59,6 +63,7 @@ int mostConnected(int** adj, int size)
 	return size - max[1];
 }
 
+
 void adjVectors(int** adj, int** adjVec, int size, int width)
 {
 	for (int i = 0; i < size; i++)
@@ -74,6 +79,84 @@ void adjVectors(int** adj, int** adjVec, int size, int width)
 		}
 	}
 }
+
+
+void edgeListFill(std::vector<std::vector<int>>& edgeList, int** adjVec, int size, int width)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (adjVec[i][j] != 0)
+			{
+				edgeList[0].push_back(i + 1);
+				edgeList[1].push_back(adjVec[i][j]);
+			}
+		}
+	}
+}
+
+
+void adjListFill(std::vector<std::vector<int>>& adjList, int** adjVec, int size, int width)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (adjVec[i][j] != 0)
+			{
+				adjList[i].push_back(adjVec[i][j]);
+			}
+		}
+	}
+}
+
+
+int incMatrix(std::vector<std::vector<int>>& inc, int** adjVec, int size, int width, bool type)
+{
+	int tail;
+	if (type)
+		tail = 1;
+	else
+		tail = -1;
+	int edges = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			bool match = 0;
+			if (adjVec[i][j] != 0)
+			{
+				if (type && edges != 0)
+				{
+					for (int f = 0; f < inc[i].size(); f++)
+					{
+						if (inc[i][f] == 1 && inc[adjVec[i][j] - 1][f] == 1)
+						{
+							match = true;
+							break;
+						}
+					}
+				}
+				if (match)
+					continue;
+				inc[i].push_back(1);
+				if(i != adjVec[i][j] - 1)
+					inc[adjVec[i][j] - 1].push_back(tail);
+				edges++;
+				for (int n = 0; n < size; n++)
+				{
+					if (n != i && (n != adjVec[i][j] - 1))
+						inc[n].push_back(0);
+				}
+			}
+		}
+	}
+
+	return edges;
+}
+
 
 int main()
 {
@@ -103,6 +186,7 @@ int main()
 	}
 
 	adjMatrix(adj, size, type);
+	std::cout << "Adjacency Matrix\n";
 	print(adj, size, size);
 	std::cout << "\n";
 
@@ -117,8 +201,35 @@ int main()
 		}
 	}
 
+	std::cout << "Adjacency Vectors\n";
 	adjVectors(adj, adjVec, size, width);
 	print(adjVec, size, width);
+	std::cout << "\n";
+
+	std::cout << "Edge List\n";
+	std::vector<std::vector<int>> edgeList (2);
+	edgeListFill(edgeList, adjVec, size, width);
+	print(edgeList, edgeList.size(), edgeList[0].size());
+	std::cout << "\n";
+
+	std::cout << "Adjacency Lists\n";
+	std::vector<std::vector<int>> adjList (size);
+	adjListFill(adjList, adjVec, size, width);
+	for (int i = 0; i < adjList.size(); i++)
+	{
+		std::cout << std::setw(2) << i+1 << "=>";
+		for (int j = 0; j < adjList[i].size(); j++)
+		{
+			std::cout << std::setw(2) << adjList[i][j];
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+
+	std::cout << "Incidence Matrix\n";
+	std::vector<std::vector<int>> inc(size);
+	int edges = incMatrix(inc, adjVec, size, width, type);
+	print(inc, size, edges);
 
 	return 0;
 }
